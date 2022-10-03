b6526003520379ba76542a85cbd37d319d79bd2b
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-10-04 03:20:38 +0800

    Final version

---
+++ app.js
@@ -0,0 +1,32 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+// routes setting
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const filterrestaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name_en.toLowerCase().includes(keyword.toLowerCase()) || restaurant.name.includes(keyword) || restaurant.category.includes(keyword)
+  })
+  res.render('index', { restaurants: filterrestaurants, keyword: keyword })
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
+// setting static files
+
