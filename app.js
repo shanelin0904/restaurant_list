6afa14211cfd8b8ae349209c6d692db0f6ed6e60
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-10-10 23:31:44 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
+//設定資料庫連線
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
