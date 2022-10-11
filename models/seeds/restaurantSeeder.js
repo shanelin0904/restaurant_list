13137f1bb3e7ee0a8b66be20ec66b1d6edf5f440
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-10-11 20:55:39 +0800

    feat: add restaurant seeder

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant') // 載入 restaurant model
+const restaurantList = require("../../restaurant.json").results// 載入種子json
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log("running restaurantSeeder script...")
+
+  Restaurant.create(restaurantList)
+    .then(() => {
+      console.log("restaurantSeeder done!")
+      db.close()
+    })
+    .catch(err => console.log(err))
+})
