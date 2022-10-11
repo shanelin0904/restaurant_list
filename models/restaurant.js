244ca77bf79fe4bd1483b6c5ea78bd485021bd30
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-10-11 20:58:32 +0800

    Modify Schema

--- models/restaurant.js
+++ models/restaurant.js
@@ -5,8 +5,37 @@ const restaurantSchema = new Schema({
     type: String, // 資料型別是字串
     required: true // 這是個必填欄位
   },
-  done: {
-    type: Boolean
+  name_en: {
+    type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
+  },
+  category: {
+    type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
+  },
+  image: {
+    type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
+  },
+  location: {
+    type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
+  },
+  phone: {
+    type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
+  },
+  google_map: {
+    type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
+  },
+  rating: {
+    type: Number, // 資料型別是字串
+    required: true // 這是個必填欄位
+  },
+  description: {
+    type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
   }
 })
 module.exports = mongoose.model('Restaurant', restaurantSchema)
