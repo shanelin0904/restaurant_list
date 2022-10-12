47b77124e6d198248436657074ceab49ccd3d368
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-10-12 15:51:46 +0800

    Setting routes

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-// require packages used in the project
+// 載入專案需要用到的框架和工具包(Packages) 
 const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
-//載入restaurant model
+//載入restaurant 資料庫
 const Restaurant = require('./models/restaurant.js')
-//設定資料庫連線
+//設定資料庫連線(但為什麼是在這裡連線??)
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -20,32 +19,91 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected')
 })
-// setting template engine
+//設定樣板引擎
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
-// routes setting
+//這是設定啥
+app.use(express.urlencoded({ extended: true }))
+
+
+// 所有路由的設定
+
+// 瀏覽全部餐廳
 app.get('/', (req, res) => {
   Restaurant.find() // 取出 Restaurant model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .then(restaurants => res.render('index', { restaurants })) // 將資料傳給 index 樣板
+    .then(restaurantsData => res.render('index', { restaurantsData })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant })
-})
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const filterrestaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name_en.toLowerCase().includes(keyword.toLowerCase()) || restaurant.name.includes(keyword) || restaurant.category.includes(keyword)
-  })
-  res.render('index', { restaurants: filterrestaurants, keyword: keyword })
+
+// 查看特定餐廳
+app.get("/restaurants/:restaurantId", (req, res) => {
+  const { restaurantId } = req.params
+  Restaurant.findById(restaurantId)
+    .lean()
+    .then(restaurant => res.render("show", { restaurant }))
+    .catch(err => console.log(err))
 })
 
-// start and listen on the Express server
+//搜尋餐廳
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  Restaurant.find({})
+    .lean()
+    .then(restaurantsData => {
+      const filterRestaurants = restaurantsData.filter(restaurant => {
+        return restaurant.name_en.toLowerCase().includes(keyword.toLowerCase()) || restaurant.name.includes(keyword) || restaurant.category.includes(keyword)
+      })
+
+      res.render('index', { restaurantsData: filterRestaurants, keyword: keyword })
+    })
+    .catch(err => console.log(err))
+  })
+
+
+// 進入「新增」餐廳頁面
+app.get("/restaurants/new", (req, res) => {
+  res.render("new")
+})
+
+// 新增餐廳
+app.post("/restaurants", (req, res) => {
+  Restaurant.create(req.body)
+    .then(() => res.redirect("/"))
+    .catch(err => console.log(err))
+})
+
+
+// 進入「編輯」餐廳頁面
+app.get("/restaurants/:restaurantId/edit", (req, res) => {
+  const { restaurantId } = req.params
+  Restaurant.findById(restaurantId)
+    .lean()
+    .then(restaurantData => res.render("edit", { restaurantData }))
+    .catch(err => console.log(err))
+})
+
+// 編輯餐廳
+app.put("/restaurants/:restaurantId", (req, res) => {
+  const { restaurantId } = req.params
+  Restaurant.findByIdAndUpdate(restaurantId, req.body)
+    //可依照專案發展方向自定編輯後的動作，這邊是導向到瀏覽特定餐廳頁面
+    .then(() => res.redirect(`/restaurants/${restaurantId}`))
+    .catch(err => console.log(err))
+})
+
+// 刪除餐廳
+app.delete("/restaurants/:restaurantId", (req, res) => {
+  const { restaurantId } = req.params
+  Restaurant.findByIdAndDelete(restaurantId)
+    .then(() => res.redirect("/"))
+    .catch(err => console.log(err))
+})
+
+// 啟動伺服器
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
-// setting static files
+
 
