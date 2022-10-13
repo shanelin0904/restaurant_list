c3e1fa2e4bf3278548776cd08557aa34a6e5b98c
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-10-13 23:13:56 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(methodOverride('_method'))
 app.get('/', (req, res) => {
   Restaurant.find() // 取出 Restaurant model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({ _id: 'desc' })
     .then(restaurantsData => res.render('index', { restaurantsData })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
