cc0a9d6206471c01ec0d6eb7858c65746f2eb888
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-11-03 15:03:28 +0800

    feat: add user modules and login page

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,14 @@ const home = require('./modules/home')
 router.use('/', home)
 // 引入 restaurant 模組程式碼
 const restaurant = require('./modules/restaurant')
-// 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurant模組
+// 將網址結構符合 /restaurants 字串開頭的 request 導向restaurant模組
 router.use('/restaurants', restaurant)
 const search = require('./modules/search')
-// 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurant模組
+// 將網址結構符合 /search 字串開頭的 request 導向search模組
 router.use('/search', search)
-
+//引入 user模組
+const users = require('./modules/users')  // add this
+// 將網址結構符合 /users 字串開頭的 request 導向users模組
+router.use('/users', users)  // add this
 // 匯出路由器
 module.exports = router
