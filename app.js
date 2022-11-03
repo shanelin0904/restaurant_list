8bec023cb8ca725ba5a9ecc184717d717fa7a92b
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-11-04 00:01:16 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const bodyParser = require('body-parser')
 // 引用路由器
 const routes = require('./routes')
@@ -28,11 +29,15 @@ app.use(methodOverride('_method'))
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
 
+app.use(flash())  // 掛載套件
+
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   console.log(req.user)
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 
