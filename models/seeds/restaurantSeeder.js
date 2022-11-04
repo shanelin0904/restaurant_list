012f2650e5cd5e84b7b7743a391213da0c22262b
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-11-04 22:05:44 +0800

    modify todoSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,12 +1,46 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const Restaurant = require('../restaurant') // 載入 restaurant model
-const restaurantList = require('../../restaurant.json').results// 載入種子json
+const User = require('../user')
 const db = require('../../config/mongoose')
+const restaurantList = require('../../restaurant.json').results// 載入種子json
+const seedUser = 
+  { name: '昱欣寶寶',
+    email: 'modelUser@example.com',
+    password: '12345678',   
+  
+  }
+
 db.once('open', () => {
-  console.log('running restaurantSeeder script...')
-  Restaurant.create(restaurantList)
-    .then(() => {
-      console.log('restaurantSeeder done!')
-      db.close()
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(seedUser.password, salt))
+    .then(hash => User.create({
+      name: seedUser.name,
+      email: seedUser.email,
+      password: hash
+    }))
+  
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: 8 },
+        (_, i) => Restaurant.create({ name: restaurantList[i].name, 
+          name_en: restaurantList[i].name_en, 
+          category: restaurantList[i].category, 
+          image: restaurantList[i].image, 
+          location: restaurantList[i].location, 
+          phone: restaurantList[i].phone, 
+          google_map: restaurantList[i].google_map, 
+          rating: restaurantList[i].rating, 
+          description: restaurantList[i].description, 
+          userId })
+      ))
     })
-    .catch(err => console.log(err))
-})
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
+  })
